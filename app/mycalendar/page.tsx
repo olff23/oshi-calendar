8fed76e6f5fdff3d942147812724459c673bba90
olff23 <kappa4080@gmail.com>
2024-07-20 16:04:07 +0900

    add calendar

--- app/mycalendar/page.tsx
+++ app/mycalendar/page.tsx
@@ -1,5 +1,10 @@
+'use client';
 import styles from "./page.module.css";
 import iyama from "../000385.json";
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import timeGridPlugin from '@fullcalendar/timegrid'
+
 
 export default function Home() {
   return (
@@ -14,6 +19,19 @@ export default function Home() {
             <li key={i}>{x.opponent.name}</li>
           ))}
         </ul>
+        <FullCalendar
+          plugins={[ dayGridPlugin, timeGridPlugin ]}
+          initialView="dayGridWeek"
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek'
+          }}
+          events={[
+            { title: 'event 1', date: '2024-07-22T15:00:00' },
+            { title: 'event 2', date: '2024-07-25' }
+          ]}
+        />
       </main>
     </>
   );
