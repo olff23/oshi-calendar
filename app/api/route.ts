be85722b7bff9773c5c318942c1536cc6b24d399
olff23 <kappa4080@gmail.com>
2024-07-20 22:23:48 +0900

    firestoreのRead/Write

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,15 +1,23 @@
 import { ResultIDs } from "@/utils/constants";
+import {db} from '@/app/firebase'
+
+const iyamaID = 385;
+const gamesCollection = db
+  .doc("sites/nihobkiin")
+  .collection("players")
+  .doc(`${iyamaID}`)
+  .collection("games");
 
 export async function GET() {
-  const res = await fetch('https://www.nihonkiin.or.jp/player_results/000385.json', {
-  })
-  const data = await res.json()
-  const games = data.games.map((x: {
+  const res = await fetch(`https://www.nihonkiin.or.jp/player_results/${`${iyamaID}`.padStart(6, '0')}.json`);
+  const jsonData: {games: {
     result: { name: string; mark: string; } | null;
     tournament: { name: string; };
     opponent: { name: string; dan: string; code: number | null; };
     date: string;
-  }) => {
+  }[] } = await res.json();
+
+  const games = jsonData.games.map(x => {
     let resultID = null;
     if (x.result) {
         switch (x.result.name) {
@@ -31,8 +39,16 @@ export async function GET() {
         date: x.date,
         resultID: resultID,
         resultName: x.result?.mark ?? null,
-    }
+    } as const;
   });
+
+  // TODO: 重複判定
+  for (const game of games) {
+    await gamesCollection.add(game);
+  }
+
+  const gamesSnapshot = await gamesCollection.get();
+  console.log(gamesSnapshot.docs.map(x=>x.data()));
  
   return Response.json({ games })
 }
