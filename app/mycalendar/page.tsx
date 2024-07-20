8030f72461b5d580fb3a96af1bab2df22cda1f85
olff23 <kappa4080@gmail.com>
2024-07-20 15:10:20 +0900

    create header

--- app/mycalendar/page.tsx
+++ app/mycalendar/page.tsx
@@ -1,14 +1,19 @@
-import styles from "../page.module.css";
+import styles from "./page.module.css";
 import iyama from "../000385.json";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-        <ul>
-          {iyama.games.map((x,i)=>(
-            <li key={i}>{x.opponent.name}</li>
-            ))}
-        </ul>
-    </main>
+    <>
+        <header className={styles.header}>
+            <h1 className={styles.h1}>推しカレンダー</h1>
+        </header>
+        <main className={styles.main}>
+            <ul>
+            {iyama.games.map((x,i)=>(
+                <li key={i}>{x.opponent.name}</li>
+                ))}
+            </ul>
+        </main>
+    </>
   );
 }
