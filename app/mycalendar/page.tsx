4e53638df43681a89b7724d0ca806aa587ef57ed
olff23 <kappa4080@gmail.com>
2024-07-20 15:31:33 +0900

    背景色を黒に変更

--- app/mycalendar/page.tsx
+++ app/mycalendar/page.tsx
@@ -4,16 +4,17 @@ import iyama from "../000385.json";
 export default function Home() {
   return (
     <>
-        <header className={styles.header}>
-            <h1 className={styles.h1}>推しカレンダー</h1>
-        </header>
-        <main className={styles.main}>
-            <ul>
-            {iyama.games.map((x,i)=>(
-                <li key={i}>{x.opponent.name}</li>
-                ))}
-            </ul>
-        </main>
+      <header className={styles.header}>
+        <h1 className={styles.h1}>推しカレンダー</h1>
+      </header>
+      <main className={styles.main}>
+        <h2 className={styles.h2}>あなたの推しカレンダー</h2>
+        <ul>
+          {iyama.games.map((x,i)=>(
+            <li key={i}>{x.opponent.name}</li>
+          ))}
+        </ul>
+      </main>
     </>
   );
 }
